33be368ceb55a639d18843a05e49e32f2b401c0e
Anurag-NURA <anuragrawatz3k@gmail.com>
2026-04-16 10:07:00 +0530

    chore(test): integrate jest testing library

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import cors, { CorsOptions } from "cors";
 import type {
-  Express,
-  Request,
-  Response,
-  NextFunction,
-  ErrorRequestHandler,
+    Express,
+    Request,
+    Response,
+    NextFunction,
+    ErrorRequestHandler,
 } from "express";
 
+import config from "./config/config.ts";
 import usersRoutes from "./routes/users.routes.ts";
 import placesRoutes from "./routes/places.routes.ts";
 
@@ -16,26 +18,39 @@ import { ErrorCode } from "./exceptions/root.ts";
 
 const app: Express = express();
 
+const options: CorsOptions = {
+    origin: config.ALLOWED_ORIGINS,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "Authorization",
+    ],
+    credentials: true,
+};
+app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept",
+    );
+    next();
 });
 
 app.use("/api", (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "Welcome to YourPlaces API",
-    routes: {
-      users: "/api/users",
-      places: "/api/places",
-    },
-  });
+    res.status(200).json({
+        message: "Welcome to YourPlaces API",
+        routes: {
+            users: "/api/users",
+            places: "/api/places",
+        },
+    });
 });
 
 app.use("/api/users", usersRoutes);
@@ -43,15 +58,15 @@ app.use("/api/places", placesRoutes);
 
 // Catch-all route for undefined routes
 app.use((req: Request, res: Response, next: NextFunction) => {
-  const error = new BadRequestException(
-    "Route not found",
-    404,
-    ErrorCode.ROUTE_NOT_FOUND,
-    {
-      route: req.originalUrl,
-    }
-  );
-  next(error);
+    const error = new BadRequestException(
+        "Route not found",
+        404,
+        ErrorCode.ROUTE_NOT_FOUND,
+        {
+            route: req.originalUrl,
+        },
+    );
+    next(error);
 });
 
 // Error handling middleware
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,13 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface Config {
-  PORT: number;
-  NODE_ENV: string;
+    PORT: number;
+    NODE_ENV: string;
+    ALLOWED_ORIGINS: string;
 }
 
 const config: Config = {
-  PORT: Number(process.env.PORT) || 3000,
-  NODE_ENV: process.env.NODE_ENV || "development",
+    PORT: Number(process.env.PORT) || 3000,
+    NODE_ENV: process.env.NODE_ENV || "development",
+    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS || "http://localhost:5173",
 };
 
 export default config;
